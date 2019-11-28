0b61676a66ad6e1ec4e0909df2b33fb7439ffff2
Marie Gandrille <57939825+mgandrille@users.noreply.github.com>
2019-11-28 11:18:59 +0100

    Add files via upload
    Amélioration suite différents bugs

--- ZCasino/ZCasino_ameliore.py
+++ ZCasino/ZCasino_ameliore.py
@@ -1,85 +1,81 @@
-# numéro choisi par le joueur
-def num_joueur():
-        n=1
-        while n!=0:
-                num_joueur = input("Sur quel numéro souhaitez vous miser (entre 0 et 49) ? : ")
-                num_joueur = int(num_joueur)
-                if num_joueur > 49 :                        
-                        print("Le numéro choisi n'existe pas, veuillez en choisir un autre !")
-                        n=1
-                else :
-                        num_joueur=int(num_joueur)
-                        n=0     
-
-# définir la couleur du numéro
-def couleur(n):
-        if  n %2 == 0 :
-                couleur == True   #noir= numéros pairs
-                print("Et c'est un numéro noir (pair)")
-        else :
-                couleur == False  #rouge= numéros impairs
-                print("Et c'est un numéro rouge (impair)")
-
-# définir les gains
-def gains():
-        import math
-        if jeu != num_joueur :
-            if (jeu %2 == 0 and num_joueur %2 == 0) or (jeu %2 != 0 and num_joueur %2 != 0):
-#            if couleur(jeu) == couleur(num_joueur) :
-                gain = math.ceil(mise * 0.5)
-                print("\n Vous avez misé sur la bonne couleur, vous gagnez ", gain, "$")
-            else:
-                gain = 0
-                print("\n Loupé, Vous venez de perdre votre mise !")
-        else:
-                gain = 3 * mise
-                print("\n Votre gain est de 3 fois votre mise, soit ", gain, "$")
- 
-
 # ****** programme du jeu de la roulette **********
 print("Bienvenue au ZCasino, voici le jeu de la Roulette !")
 import random
 import math
+# Somme disponible au départ
 a = input("\nAvec quelle somme d'argent souhaitez vous jouer ? ")
-a = int(a)
+while type(a) != int:
+        try:
+                int(a) == True
+        except ValueError:
+                print("Il y a une erreur, êtes vous certain de votre frappe ?")
+                a = input("Avec quelle somme d'argent souhaitez vous jouer ? ")
+        else :
+                a = int(a)
+                if a < 1 :
+                        print("Vous ne pouvez pas jouer sans argent !")
+                        a = input("Avec quelle somme d'argent souhaitez vous jouer ? ")
+# début du jeu
 j=1
 while j != 0 :
-    #num_joueur()
-    num_joueur = input("\nSur quel numéro souhaitez vous miser (entre 0 et 49) ? : ")
-    num_joueur = int(num_joueur)
-    # choisir sa mise
-    mise = input("Quelle somme souhaitez vous miser sur ce numéro ? : ")
-    mise = int(mise)
-    print("\n Votre mise est de ", mise, "$ sur le numéro ", num_joueur, "\n")
-    # lance la roulette
-    print("A vos jeux... La roulette est lancée... \n")
-    jeu=random.randrange(50)
-    print("Le numéro gagnant est le numéro ", jeu)
-    #couleur(jeu)
-    #   gains()
-    if jeu != num_joueur :
-        if (jeu %2 == 0 and num_joueur %2 == 0) or (jeu %2 != 0 and num_joueur %2 != 0):
-            gain = math.ceil(mise * 0.5)
-            print("\n Vous avez misé sur la bonne couleur, vous gagnez ", gain, "$")
-            a = a + gain
+        # numéro choisi par le joueur
+        num_joueur = input("\nSur quel numéro souhaitez vous miser (entre 0 et 49) ? : ")
+        while type(num_joueur) != int:
+                try:
+                        int(num_joueur) == True
+                except ValueError:
+                        print("Il y a une erreur, êtes vous certain de votre frappe ?")
+                        num_joueur = input("Sur quel numéro souhaitez vous miser (entre 0 et 49) ? : ")
+                else :
+                        num_joueur = int(num_joueur)
+                        if num_joueur < 0 or num_joueur > 49:
+                                print("Le numéro choisi n'existe pas, veuillez en choisir un autre !")
+                                num_joueur = input("Sur quel numéro souhaitez vous miser (entre 0 et 49) ? : ")
+        # choisir sa mise
+        mise = input("\nQuelle somme souhaitez vous miser sur ce numéro ? : ")
+        while type(mise) != int :
+                try:
+                        int(mise) == True
+                except ValueError :
+                        print("Etes vous certain de votre frappe ?")
+                        mise = input("Quelle somme souhaitez vous miser sur ce numéro ? : ")
+                else :
+                        mise = int(mise)
+                        if mise <0:
+                                print("Vous ne pouvez pas miser un nombre négatif !")
+                                mise = input("Quelle somme souhaitez vous miser sur ce numéro ? : ") 
+                        elif mise >a:
+                                print("Vous n'avez pas assez d'argent !")
+                                mise = input("Quelle somme souhaitez vous miser sur ce numéro ? : ")
+        print("\n Votre mise est de ", mise, "$ sur le numéro ", num_joueur, "\n")
+        # lance la roulette
+        print("A vos jeux... La roulette est lancée... \n")
+        jeu=random.randrange(50)
+        print("Le numéro gagnant est le numéro ", jeu)
+        # résultats et gains
+        if jeu != num_joueur :
+                if (jeu %2 == 0 and num_joueur %2 == 0) or (jeu %2 != 0 and num_joueur %2 != 0):
+                        gain = math.ceil(mise * 0.5)
+                        print("\n Vous avez misé sur la bonne couleur, vous gagnez ", gain, "$")
+                        a = a + gain
+                else:
+                        gain = 0
+                        print("\n Loupé, Vous venez de perdre votre mise !")
+                        a = a - mise
         else:
-            gain = 0
-            print("\n Loupé, Vous venez de perdre votre mise !")
-            a = a - mise
-    else:
-            gain = 3 * mise
-            print("\n Votre gain est de 3 fois votre mise, soit ", gain, "$")
-            a = a + gain
-    print("Et vous avez au total ", a, " $")
-    # possibilité de rejouer ou non
-    if a == 0:
-        print("\nVous n'avez plus d'argent ! Au revoir et à bientôt au ZCasino ")
-        j=0
-    else:
-        r=input(" Voulez vous rejouer ? (O / N) ")
-        if r == "O" or r == "o" :
-                 j=1
-        else:
-                print("Au revoir et à bientôt au ZCasino ")
+                gain = 3 * mise
+                print("\n Votre gain est de 3 fois votre mise, soit ", gain, "$")
+                a = a + gain
+        print("Il vous reste donc ", a, " $ pour continuer de jouer.")
+        # possibilité de rejouer ou non
+        if a == 0:
+                print("\nVous n'avez plus d'argent ! Au revoir et à bientôt au ZCasino ")
                 j=0
+        else:
+                r=input(" Voulez vous rejouer ? (O / N) ")
+                if r == "O" or r == "o" :
+                        j=1
+                else:
+                        print("Au revoir et à bientôt au ZCasino ")
+                        j=0
 
