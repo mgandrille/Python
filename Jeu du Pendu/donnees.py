5720af1fe6c944b9d9466b9a76caa7e2a23f641a
Marie Gandrille <57939825+mgandrille@users.noreply.github.com>
2019-12-10 15:53:17 +0100

    TP_Jeu du Pendu

---
+++ Jeu du Pendu/donnees.py
@@ -0,0 +1,15 @@
+
+mot_mystere = ["vision",
+                        "bateau",
+                        "demain",
+                        "bijoux",
+                        "chouette",
+                        "bonjour",
+                        "amour",
+                        "joie",
+                        "festival"]
+
+joueur = []
+
+
+score = []
